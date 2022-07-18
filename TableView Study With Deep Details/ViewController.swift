5363d4de9e5f96f58ca6b0f82425ad6c32ad91a2
buuraak77 <brkylmz8@hotmail.com>
2022-07-18 16:49:38 +0300

    Initial Commit

---
+++ TableView Study With Deep Details/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView Study With Deep Details
+//
+//  Created by Burak Yılmaz on 18.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
